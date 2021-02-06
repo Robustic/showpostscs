4bae5f4b2e46b90edbab2de1bee689c0d7fb98f6
Juha M <juhas.malinen@gmail.com>
2021-02-06 13:58:38 +0200

    First commit

---
+++ ShowPostsCs/RestSharpConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ShowPostsCs
+{
+    public class PostClass
+    {
+        public string Date { get; set; }
+        public string Topic { get; set; }
+        public string Type2 { get; set; }
+        public string Title { get; set; }
+        public int Views { get; set; }
+        public int Likes { get; set; }
+        public int Comments { get; set; }
+    }
+
+    public class RestSharpConnection
+    {
+        public static List<PostClass> PrintJSON()
+        {
+            var client = new RestClient("https://guarded-mountain-76807.herokuapp.com");
+            var request = new RestRequest("api/posts", Method.GET);
+            
+            // Add HTTP headers
+            request.AddHeader("User-Agent", "Nothing");          
+
+            // Execute the request and automatically deserialize the result.
+            var posts = client.Execute<List<PostClass>>(request);
+
+            return posts.Data;
+        }
+    }
+}
